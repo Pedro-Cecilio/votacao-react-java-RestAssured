a3357db41bfbb83b05acdf0f1d859bd82fdb2598
Pedro-Cecilio <cardev604@gmail.com>
2024-05-04 01:28:37 -0300

    :recycle: refactor: refatoreando classe de teste AbrirSessaoVotacaoTest, utilizando métodos do TestUtils

--- src/test/java/com/dbserver/restassured/sessaoVotacao/AbrirSessaoVotacaoTest.java
+++ src/test/java/com/dbserver/restassured/sessaoVotacao/AbrirSessaoVotacaoTest.java
@@ -1,7 +1,6 @@
 package com.dbserver.restassured.sessaoVotacao;
 
 import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.port;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
@@ -18,137 +17,91 @@ import com.dbserver.restassured.fixture.LoginFixture;
 import com.dbserver.restassured.models.auth.LoginEnvio;
 import com.dbserver.restassured.models.pauta.CriarPautaDados;
 import com.dbserver.restassured.models.sessaoVotacao.AbrirSessaoVotacaoDados;
+import com.dbserver.restassured.utils.TestUtils;
 
-import io.restassured.http.ContentType;
 
 @SpringBootTest
 class AbrirSessaoVotacaoTest {
-    private String token;
-    private Integer pautaId;
-    private static final String ENDPOINT = "votacao/abrir";
+        private static String tokenAdmin;
+        private Integer pautaId;
 
-    @BeforeAll
-    static void setUp() {
-        baseURI = "http://localhost/";
-        port = 8080;
-    }
+        @BeforeAll
+        static void setUp() {
+                baseURI = "http://localhost/";
+                port = 8080;
+                LoginEnvio dadosLogin = LoginFixture.dadosLoginAdminValido();
+                tokenAdmin = TestUtils.fazerLoginEObterToken(dadosLogin);
+        }
 
-    @BeforeEach
-    void configurar() {
-        LoginEnvio dadosLogin = LoginFixture.dadosLoginAdminValido();
-        CriarPautaDados novaPauta = CriarPautaFixture.criarPautaCorretamente();
+        @BeforeEach
+        void configurar() {
+                CriarPautaDados novaPauta = CriarPautaFixture.criarPautaCorretamente();
+                this.pautaId = TestUtils.cadastrarPauta(novaPauta, tokenAdmin)
+                                .then()
+                                .extract().path("id");
+        }
 
-        this.token = given()
-                .body(dadosLogin)
-                .contentType(ContentType.JSON)
-                .when()
-                .post("/auth/login")
-                .then()
-                .extract().path("token");
+        @Test
+        void dadosEstouLogadoComoAdminQuandoAbroSessaoVotacaoEntaoDeveRetornarStatus200() {
+                AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
+                                .abrirSessaoVotacaoCorretamente(this.pautaId);
 
-        this.pautaId = given()
-                .header("Authorization", token)
-                .body(novaPauta)
-                .contentType(ContentType.JSON)
-                .when()
-                .post("pauta")
-                .then()
-                .extract().path("id");
-    }
+                TestUtils.abrirSessaoVotacao(dadosAbrirSessaoVotacao, tokenAdmin)
+                                .then()
+                                .statusCode(HttpStatus.SC_OK)
+                                .and().assertThat().body("id", notNullValue())
+                                .and().assertThat().body("pautaId", equalTo(this.pautaId));
+        }
 
-    @Test
-    void dadosEstouLogadoComoAdminQuandoAbroSessaoVotacaoEntaoDeveRetornarStatus200() {
-        AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
-                .abrirSessaoVotacaoCorretamente(this.pautaId);
-        given()
-                .header("Authorization", token)
-                .body(dadosAbrirSessaoVotacao)
-                .contentType(ContentType.JSON)
-                .when()
-                .post(ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .and().assertThat().body("id", notNullValue())
-                .and().assertThat().body("pautaId", equalTo(this.pautaId));
-    }
+        @Test
+        void dadosEstouLogadoComoAdminQuandoAbroSessaoVotacaoEmPautaInexistenteEntaoDeveRetornarStatus404() {
+                AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
+                                .abrirSessaoVotacaoPautaInexistente();
 
-    @Test
-    void dadosEstouLogadoComoAdminQuandoAbroSessaoVotacaoEmPautaInexistenteEntaoDeveRetornarStatus404() {
-        AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
-                .abrirSessaoVotacaoPautaInexistente();
-        given()
-                .header("Authorization", token)
-                .body(dadosAbrirSessaoVotacao)
-                .contentType(ContentType.JSON)
-                .when()
-                .post(ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.SC_NOT_FOUND)
-                .and().assertThat().body("erro", equalTo("Pauta não encontrada."));
-    }
+                TestUtils.abrirSessaoVotacao(dadosAbrirSessaoVotacao, tokenAdmin)
+                                .then()
+                                .statusCode(HttpStatus.SC_NOT_FOUND)
+                                .and().assertThat().body("erro", equalTo("Pauta não encontrada."));
+        }
 
-    @Test
-    void dadosEstouLogadoComoAdminQuandoAbroSessaoVotacaoMinutosInvalidosEntaoDeveRetornarStatus404() {
-        AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
-                .abrirSessaoVotacaoMinutosInvalido(this.pautaId);
-        given()
-                .header("Authorization", token)
-                .body(dadosAbrirSessaoVotacao)
-                .contentType(ContentType.JSON)
-                .when()
-                .post(ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .and().assertThat().body("erro", equalTo("Minutos deve ser maior que 0."));
-    }
+        @Test
+        void dadosEstouLogadoComoAdminQuandoAbroSessaoVotacaoMinutosInvalidosEntaoDeveRetornarStatus404() {
+                AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
+                                .abrirSessaoVotacaoMinutosInvalido(this.pautaId);
 
-    @Test
-    void dadosEstouLogadoComoUsuarioQuandoTentoAbrirSessaoVotacaoEntaoDeveRetornarStatus403() {
-        AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
-                .abrirSessaoVotacaoMinutosInvalido(this.pautaId);
+                TestUtils.abrirSessaoVotacao(dadosAbrirSessaoVotacao, tokenAdmin)
+                                .then()
+                                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                                .and().assertThat().body("erro", equalTo("Minutos deve ser maior que 0."));
+        }
 
-        LoginEnvio dadosLogin = LoginFixture.dadosLoginUsuarioValido();
+        @Test
+        void dadosEstouLogadoComoUsuarioQuandoTentoAbrirSessaoVotacaoEntaoDeveRetornarStatus403() {
+                AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
+                                .abrirSessaoVotacaoMinutosInvalido(this.pautaId);
 
-        this.token = given()
-                .body(dadosLogin)
-                .contentType(ContentType.JSON)
-                .when()
-                .post("/auth/login")
-                .then()
-                .extract().path("token");
+                LoginEnvio dadosLogin = LoginFixture.dadosLoginUsuarioValido();
 
-        given()
-                .header("Authorization", token)
-                .body(dadosAbrirSessaoVotacao)
-                .contentType(ContentType.JSON)
-                .when()
-                .post(ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.SC_FORBIDDEN);
-    }
+                String tokenUsuario = TestUtils.fazerLoginEObterToken(dadosLogin);
 
-    @Test
-    void dadosEstouLogadoComoAdminQuandoAbroSessaoVotacaoJaAbertaEntaoDeveRetornarStatus400() {
-        AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
-                .abrirSessaoVotacaoCorretamente(this.pautaId);
-        given()
-                .header("Authorization", token)
-                .body(dadosAbrirSessaoVotacao)
-                .contentType(ContentType.JSON)
-                .when()
-                .post(ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.SC_OK);
+                TestUtils.abrirSessaoVotacao(dadosAbrirSessaoVotacao, tokenUsuario)
+                                .then()
+                                .statusCode(HttpStatus.SC_FORBIDDEN);
+        }
 
-        given()
-                .header("Authorization", token)
-                .body(dadosAbrirSessaoVotacao)
-                .contentType(ContentType.JSON)
-                .when()
-                .post(ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .and().assertThat().body("erro", equalTo("Pauta já possui uma votação aberta."));
-                
-    }
+        @Test
+        void dadosEstouLogadoComoAdminQuandoAbroSessaoVotacaoJaAbertaEntaoDeveRetornarStatus400() {
+                AbrirSessaoVotacaoDados dadosAbrirSessaoVotacao = AbrirSessaoVotacaoFixture
+                                .abrirSessaoVotacaoCorretamente(this.pautaId);
+
+                TestUtils.abrirSessaoVotacao(dadosAbrirSessaoVotacao, tokenAdmin)
+                                .then()
+                                .statusCode(HttpStatus.SC_OK);
+
+                TestUtils.abrirSessaoVotacao(dadosAbrirSessaoVotacao, tokenAdmin)
+                                .then()
+                                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                                .and().assertThat().body("erro", equalTo("Pauta já possui uma votação aberta."));
+
+        }
 }
